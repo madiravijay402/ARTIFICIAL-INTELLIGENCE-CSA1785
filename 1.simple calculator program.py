ede165be33bedf35048b9902ac32c84309196a9b
madiravijay402 <113880158+madiravijay402@users.noreply.github.com>
2023-01-11 08:26:47 +0530

    Create 1.simple calculator program.py

---
+++ 1.simple calculator program.py
@@ -0,0 +1,28 @@
+a=int(input("enter the number"))
+b=int(input("enter the number"))
+c=input("enter the choice")
+if(c=="+"):
+    print(a+b)
+elif(c=="-"):
+    print(a-b)
+elif(c=="*"):
+    print(a*b)
+elif(c=="/"):
+    print(a/b)
+else:
+    print("invalid")
+e=input("need to do other operations")
+while e=="yes":
+    a=int(input("enter the number"))
+    b=int(input("enter the number"))
+    c=input("enter the choice")
+    if(c=="+"):
+        print(a+b)
+    elif(c=="-"):
+        print(a-b)
+    elif(c=="*"):
+        print(a*b)
+    elif(c=="/"):
+        print(a/b)
+    e=input("need to do other operations")
+    

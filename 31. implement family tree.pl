dd26567f1d9734474f329bb52b73975d5ea8ddbd
madiravijay402 <113880158+madiravijay402@users.noreply.github.com>
2023-01-20 16:27:12 +0530

    Create 31. implement family tree.pl

---
+++ 31. implement family tree.pl
@@ -0,0 +1,24 @@
+% Mother relation
+mother(Pam, Liz).
+mother(Pam, Ann).
+mother(Pam, Pat).
+
+% Father relation
+father(Tom, Liz).
+father(Tom, Ann).
+father(Tom, Pat).
+
+% Grandfather relation
+grandfather(Tom, Bob).
+grandfather(Tom, Jim).
+
+% Grandmother relation
+grandmother(Pam, Bob).
+grandmother(Pam, Jim).
+
+% Sister relation
+sister(Liz, Ann).
+sister(Liz, Pat).
+
+% Brother relation
+brother(Bob, Jim).
